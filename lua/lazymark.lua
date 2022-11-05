ae7c3ff4abeb386bb514eb3d275fc24d78d814f3
Lintao <Lintao.Amons@outlook.com>
2022-11-05 19:04:32 +0800

    Can't find commands at the startup?

--- lua/lazymark.lua
+++ lua/lazymark.lua
@@ -7,25 +7,26 @@ M.setup = function()
 end
 
 M.mark = function()
-  print("start mark")
   local row, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-  print(row, col)
   local abspath = vim.api.nvim_buf_get_name(0)
   local mark = abspath .. "|" .. row .. "," .. col
-  print(mark)
   vim.cmd(":call writefile" .. '(["' .. mark .. '"],"' .. markPersistency .. '")')
 end
 
-local commands = {
-  {
-    name = "Lazymark",
-    callback = M.mark,
-  },
-}
+M.initCommands = function()
+  local commands = {
+    {
+      name = "Lazymark",
+      callback = M.mark,
+    },
+  }
 
-for _, v in ipairs(commands) do
-  print("setup commands")
-  vim.api.nvim_create_user_command(v.name, v.callback, {})
+  for _, v in ipairs(commands) do
+    print("setup commands")
+    vim.api.nvim_create_user_command(v.name, v.callback, {})
+  end
 end
+
+M.initCommands()
 
 return M
