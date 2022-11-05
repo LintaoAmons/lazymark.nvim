bfda3ad90afc0dc8525862ea88f7d9b9f942cf7b
Lintao <Lintao.Amons@outlook.com>
2022-11-05 21:58:57 +0800

    impl gotoMark

--- lua/lazymark.lua
+++ lua/lazymark.lua
@@ -1,37 +1,49 @@
 local M = {}
 
+local commands = {
+	{
+		name = "Lazymark",
+		callback = M.mark,
+	},
+	{
+		name = "LazymarkGoToMark",
+		callback = M.goToMark,
+	},
+}
+
 local markPersistency = vim.fn.stdpath("cache") .. "/lazymark.nvim"
 
-M.setup = function()
-  print("Hello lazymark")
+local function parseMark(mark)
+	local parsedResult = {}
+	for filename, position in string.gmatch(mark, "(.+)|(.+)") do
+		parsedResult.filename = filename
+		for row, col in string.gmatch(position, "(.+),(.+)") do
+			local rowCol = {}
+			table.insert(rowCol, tonumber(row))
+			table.insert(rowCol, tonumber(col))
+			parsedResult.location = rowCol
+		end
+	end
+	return parsedResult
 end
 
-M.readFile = function()
-  local test = vim.fn.readfile(markPersistency)[0]
-  print(test)
+M.goToMark = function()
+	local persistedMark = vim.fn.readfile(markPersistency)[1]
+	local parsedMark = parseMark(persistedMark)
+	vim.cmd(":e " .. parsedMark.filename)
+	vim.api.nvim_win_set_cursor(0, parsedMark.location)
 end
 
 M.mark = function()
-  local row, col = table.unpack(vim.api.nvim_win_get_cursor(0))
-  local abspath = vim.api.nvim_buf_get_name(0)
-  local mark = abspath .. "|" .. row .. "," .. col
-  vim.cmd(":call writefile" .. '(["' .. mark .. '"],"' .. markPersistency .. '")')
+	local row, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+	local abspath = vim.api.nvim_buf_get_name(0)
+	local mark = abspath .. "|" .. row .. "," .. col
+	vim.cmd(":call writefile" .. '(["' .. mark .. '"],"' .. markPersistency .. '")')
 end
 
-M.initCommands = function()
-  local commands = {
-    {
-      name = "Lazymark",
-      callback = M.mark,
-    },
-  }
-
-  for _, v in ipairs(commands) do
-    print("setup commands")
-    vim.api.nvim_create_user_command(v.name, v.callback, {})
-  end
+for _, v in ipairs(commands) do
+	print("setup commands")
+	vim.api.nvim_create_user_command(v.name, v.callback, {})
 end
-
-M.initCommands()
 
 return M
