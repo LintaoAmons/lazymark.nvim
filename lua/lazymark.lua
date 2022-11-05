25f99337403eac69ad9a13ad27536e1d0e5ee2c3
Lintao <Lintao.Amons@outlook.com>
2022-11-05 18:45:20 +0800

    mark

--- lua/lazymark.lua
+++ lua/lazymark.lua
@@ -1,7 +1,16 @@
 local M = {}
 
+local markPersistency = vim.fn.stdpath("cache") .. "/lazymark.nvim"
+
 M.setup = function()
   print("Hello lazymark")
+end
+
+M.mark = function()
+  local row, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+  local abspath = vim.api.nvim_buf_get_name(0)
+  local mark = abspath .. "|" .. row .. "," .. col
+  vim.cmd(":call writefile" .. '(["' .. mark .. '"],"' .. markPersistency .. '")')
 end
 
 return M
