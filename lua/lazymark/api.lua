1a825401c6108d0921b36fb6299ae9283c30c518
Lintao <Lintao.Amons@outlook.com>
2022-11-26 10:26:22 +0800

    [refactor] encapture mark-string logic

--- lua/lazymark/api.lua
+++ lua/lazymark/api.lua
@@ -1,3 +1,5 @@
+local MarkString = require("lazymark.mark-string")
+
 local M = {}
 
 table.unpack = table.unpack or unpack
@@ -18,36 +20,17 @@ local function getDoMarkHistory()
 	return vim.fn.readfile(do_mark_history)[1]
 end
 
--- todo: markString related into a seperate file
-local function parseMark(markString)
-	local parsedResult = {}
-	for filename, position in string.gmatch(markString, "(.+)|(.+)") do
-		parsedResult.filename = filename
-		for row, col in string.gmatch(position, "(.+),(.+)") do
-			local rowCol = {}
-			table.insert(rowCol, tonumber(row))
-			table.insert(rowCol, tonumber(col))
-			parsedResult.location = rowCol
-		end
-	end
-	return parsedResult
-end
-
-local function buildMarkString(abspath, row, col)
-	return abspath .. "|" .. row .. "," .. col
-end
-
 local function addMark(row, col)
 	initDir()
 
-	local markString = buildMarkString(vim.api.nvim_buf_get_name(0), row, col)
+	local markString = MarkString.build(vim.api.nvim_buf_get_name(0), row, col)
 	-- todo: append new mark in last line of file
 	vim.cmd(":call writefile" .. '(["' .. markString .. '"],"' .. do_mark_history .. '")')
 end
 
 M.gotoMark = function()
-	local rawMark = getDoMarkHistory()
-	local parsedMark = parseMark(rawMark)
+	local markString = getDoMarkHistory()
+	local parsedMark = MarkString.parse(markString)
 	vim.cmd(":e " .. parsedMark.filename)
 	vim.api.nvim_win_set_cursor(0, parsedMark.location)
 end
---
+++ lua/lazymark/mark-string.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+-- parsedMark example:
+-- {
+--   filename = "/Users/lintao.zhang/.config/nvim/lua/user/my-plugin/init.lua",
+--   location = { 30, 4 }
+-- }
+M.parse = function(markString)
+	local parsedMark = {}
+	for filename, position in string.gmatch(markString, "(.+)|(.+)") do
+		parsedMark.filename = filename
+		for row, col in string.gmatch(position, "(.+),(.+)") do
+			local rowCol = {}
+			table.insert(rowCol, tonumber(row))
+			table.insert(rowCol, tonumber(col))
+			parsedMark.location = rowCol
+		end
+	end
+	return parsedMark
+end
+
+-- markString example: 
+-- /Users/lintao.zhang/.config/nvim/lua/user/my-plugin/init.lua|30,4
+M.build = function(abspath, row, col)
+	return abspath .. "|" .. row .. "," .. col
+end
+
+return M
