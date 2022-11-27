17ff301df0394f6619f3012979a623941ed62685
Lintao <Lintao.Amons@outlook.com>
2022-11-27 17:04:02 +0800

    add fs-utils

---
+++ lua/lazymark/fs-utils.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+local uv = vim.loop
+
+M.readFileSync = function(path)
+	local fd = assert(uv.fs_open(path, "r", 438))
+	local stat = assert(uv.fs_fstat(fd))
+	local data = assert(uv.fs_read(fd, stat.size, 0))
+	assert(uv.fs_close(fd))
+	return data
+end
+
+M.appendFileSync = function(path)
+	local fd = assert(uv.fs_open(path, "a", 438))
+	local data = assert(uv.fs_write(fd, "test_content"))
+	assert(uv.fs_close(fd))
+	return data
+end
+
+return M
